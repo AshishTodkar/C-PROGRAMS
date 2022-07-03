aa80f5e4afd5b296c06402b9ccad996d912cb150
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-07-03 06:50:37 +0530

    Add files via upload

---
+++ Array/Array (3).c
@@ -0,0 +1,19 @@
+// Partial initialisation
+
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int num[5]={15,45};
+  
+  printf("\n Value of 1st Element = %d",num[0]);
+  printf("\n Value of 2nd Element = %d",num[1]);
+  printf("\n Value of 3rd Element = %d",num[2]);
+  printf("\n Value of 4th Element = %d",num[3]);
+  printf("\n Value of 5th Element = %d",num[4]);
+  
+  
+  getch();
+  return 0;
+}
